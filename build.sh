13c688d8ffdca0acc2e36bcb7467b1e78bc70f86
lsy969999 <lsy969999@gmail.com>
2024-12-12 06:00:12 +0900

    g

--- build.sh
+++ build.sh
@@ -4,14 +4,16 @@ name="lsy969999_github_io"
 
 cargo build --release --no-default-features --target wasm32-unknown-unknown
 wasm-bindgen --out-name $name \
-  --out-dir dist \
+  --out-dir disttest \
   --target web target/wasm32-unknown-unknown/release/$name.wasm
 
 # wasm build 에의해 생성되는 fetch를 추적가능한 fetch로 바꾸기
-sed -i '' 's/module_or_path = fetch(module_or_path)/module_or_path = bevyProgressiveFetch(module_or_path)/' ./dist/${name}.js
+# sed -i '' 's/module_or_path = fetch(module_or_path)/module_or_path = bevyProgressiveFetch(module_or_path)/' ./disttest/${name}.js
 
-wasm-opt -Oz --output optimized.wasm dist/${name}_bg.wasm
-mv optimized.wasm dist/${name}_bg.wasm
+echo "`ls -al`"
+
+wasm-opt -Oz --output optimized.wasm disttest/${name}_bg.wasm
+mv optimized.wasm disttest/${name}_bg.wasm
 
 
-echo $(stat -f %z dist/${name}_bg.wasm)
+echo $(stat -f %z disttest/${name}_bg.wasm)
